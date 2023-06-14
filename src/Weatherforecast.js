4609d48a6b46849411c7d86866b2884bcd54cdb0
Bukkiedevs <bukkie92@gmail.com>
2023-06-14 10:45:54 +0200

    Improved forecast layout

--- src/Weatherforecast.js
+++ src/Weatherforecast.js
@@ -1,10 +1,20 @@
 import React from "react";
-export default function Weatherforecast() {
+import "./Weatherforecast.css";
+export default function Weatherforecast(props) {
   return (
-    <div>
+    <div className="Weatherforecast">
       <div className="row">
         <div className="col">
-          <h3>Weather forecast here!</h3>
+          <div className="dayForecast">Thu</div>
+          <img
+            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAdVJREFUaN7tmc1thDAQRimBElwCJVBCSvAxR5fgEiiBEiiBErhyIx24A2cc2WhiAf4ZA1rJkZ4UZZPN9/AwHrON1rr5ZJoqUAWqQBWoAlWgxJf++WaAAGZAAdpD2dfM7zDS/yopAGE6YDoIHMLIdK8KQIAWGIAtQ8Bh/r59bQWQjCBILCkSJIF1XVuAA9Jivm9ROd0ukS0AQTtgA7SH+Vn31EoEBSAMA2YUUAHiJDyWcCtBuidIArZEroJewVEpjQSJjiIgMsMbpHdjf53sCcEWSxEYCQKOyZQhkshZBZYkYEtHeLVPQSGJnHIS0QI2/FIo+L+VILTXOUVA3BD+D3Q/pAqoFIEebUxFQQLJN/Ojo0TEqDG/JgBv1hdgeVNAP4CKPSvkCKiCQc1KSMRs2+x902hO/Z4cYFhgWOQHY8zo9hOKgCCGH71BEXcqHjEBKDft5gowypVH4YeLgKE9ZSO10cxz7z7TFJqxOEUgZxyYbPi+0M4uSRuZPYCnCPBA6TwrYCWWyFbJImo/FTMpM6pAG5CYvDO0LDii7x2JNAtdSGxuQyp41Q87UqkHW8NJzYsbw+8d6Y5Hi+7qbw8IyOIPd9HRVD8qUD8fqAJVoApUgSrwqfwCJ6xaZshM+xMAAAAASUVORK5CYII="
+            alt="cloudy"
+            className="float-left mb-4"
+          />
+          <div className="tempratureForecast">
+            <span className="maxTemp">26°</span>
+            <span className="minTemp">14°</span>
+          </div>
         </div>
       </div>
     </div>
