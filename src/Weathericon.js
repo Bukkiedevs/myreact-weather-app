d14a9c825c7d7b96c0fe0594a13bed6ec6d64c7e
Bukkiedevs <bukkie92@gmail.com>
2023-06-16 14:44:56 +0200

    Add a default weather Icon

--- src/Weathericon.js
+++ src/Weathericon.js
@@ -1,11 +1,33 @@
 import React from "react";
 import ReactAnimatedWeather from "react-animated-weather";
+
 export default function Weathericon(props) {
+  const { code, size } = props;
+
+  const codeMapping = {
+    "01d": "CLEAR_DAY",
+    "01n": "CLEAR_NIGHT",
+    "02d": "PARTLY_CLOUDY_DAY",
+    "02n": "PARTLY_CLOUDY_NIGHT",
+    "03d": "PARTLY_CLOUDY_DAY",
+    "03n": "PARTLY_CLOUDY_NIGHT",
+    "04d": "CLOUDY",
+    "04n": "CLOUDY",
+    "09d": "RAIN",
+    "09n": "RAIN",
+    "10d": "SLEET",
+    "10n": "SLEET",
+    "13d": "SNOW",
+    "13n": "SNOW",
+    "50d": "FOG",
+    "50n": "FOG",
+  };
+  const weatherIcon = codeMapping[code] || "CLEAR_DAY";
   return (
     <ReactAnimatedWeather
-      icon={"CLEAR_DAY"}
+      icon={weatherIcon}
       color="#1e1e1e"
-      size={34}
+      size={size}
       animate={true}
     />
   );
