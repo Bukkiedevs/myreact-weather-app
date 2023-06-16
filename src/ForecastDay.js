f57bfa4a8f9254d5a7839633fcc9f95beda7f235
Bukkiedevs <bukkie92@gmail.com>
2023-06-16 14:42:11 +0200

    Create new component for daily forecast

---
+++ src/ForecastDay.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Weathericon from "./Weathericon";
+export default function ForecastDay(props) {
+  function day() {
+    let date = new Date(props.dailyForecast.dt * 1000);
+    let day = date.getDay();
+
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    return days[day];
+  }
+  return (
+    <div>
+      <div className="dayForecast">{day()}</div>
+      <Weathericon code={props.dailyForecast.weather[0].icon} size={64} />
+      <div className="tempratureForecast">
+        <span className="maxTemp">
+          {" "}
+          {Math.round(props.dailyForecast.temp.max)}°
+        </span>
+        <span className="minTemp">
+          {Math.round(props.dailyForecast.temp.min)}°
+        </span>
+      </div>
+    </div>
+  );
+}
